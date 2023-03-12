b3fcaf41fe422eb6a608acf8987cd7143d9a1ec7
kyunghwa <snoopn303@gmail.com>
2023-03-12 22:17:03 +0900

    Connectivity Manager

---
+++ app/src/main/java/sesac/part23/network/connectivity/MainActivity.kt
@@ -0,0 +1,11 @@
+package sesac.part23.network.connectivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
